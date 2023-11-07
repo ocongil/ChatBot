b5e41e3ae2bd16cc8d9d505eb2c9b8b2c20dd2f1
ocongil <47063881+ocongil@users.noreply.github.com>
2023-11-06 17:33:59 -0700

    Change the lists

---
+++ chatbotload.py
@@ -0,0 +1,77 @@
+import streamlit as st
+from llama_index import VectorStoreIndex, ServiceContext, Document,  download_loader
+from llama_index.llms import OpenAI
+import openai
+from llama_index import SimpleDirectoryReader, StorageContext, load_index_from_storage
+
+st.set_page_config(page_title="Chat with the City of Rapid City website, powered by LlamaIndex", page_icon="🦙", layout="centered", initial_sidebar_state="auto", menu_items=None)
+openai.api_key = st.secrets.openai_key
+st.title("Chat with the City of Rapid City website.")
+st.info("This is a demo using the top 150 pages from the city website", icon="📃")
+
+def read_url_list(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            url_list = file.read().splitlines()
+        return url_list
+    except FileNotFoundError:
+        print(f"Error: File not found at {file_path}")
+        return []
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return []
+    
+url_list = read_url_list(".\Data\PageList.txt")
+#url_list = read_url_list(".\Data\PageListWithCouncil.txt")
+         
+if "messages" not in st.session_state.keys(): # Initialize the chat messages history
+    st.session_state.messages = [
+        {"role": "assistant", "content": "Ask me a question about the City of Rapid City!"}
+    ]
+
+@st.cache_resource(show_spinner=False)
+# def load_data():
+#     with st.spinner(text="Loading and indexing the Streamlit docs – hang tight! This should take 1-2 minutes."):
+#         reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
+#         docs = reader.load_data()
+#         service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the Streamlit Python library and your job is to answer technical questions. Assume that all questions are related to the Streamlit Python library. Keep your answers technical and based on facts – do not hallucinate features."))
+#         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+#         return index
+# def load_data():
+#     with st.spinner(text="Loading and indexing the City of Rapid City website – hang tight! This should take 1-2 minutes."):
+#         #reader = SimpleDirectoryReader(input_dir="./data", recursive=True)
+#         BeautifulSoupWebReader = download_loader("BeautifulSoupWebReader")
+#         loader = BeautifulSoupWebReader()
+#         # docs = loader.load_data(urls=['https://www.rcgov.org/departments/mayor-s-office-city-council/mayor-s-office.html', 'https://www.rcgov.org/departments/mayor-s-office-city-council/city-council.html',
+#         #                                 'https://www.rcgov.org/departments.html', 'https://www.rcgov.org/departments/finance.html'])
+#         docs = loader.load_data(urls=url_list)
+#         #docs = reader.load_data()
+#         service_context = ServiceContext.from_defaults(llm=OpenAI(model="gpt-3.5-turbo", temperature=0.5, system_prompt="You are an expert on the City of Rapid City and your job is to answer citizen questions. Assume that all questions are related to the City of Rapid City. Keep your answers simple and based on facts – do not hallucinate features."))
+#         index = VectorStoreIndex.from_documents(docs, service_context=service_context)
+#         return index
+def load_data():
+    with st.spinner(text="Loading and indexing the City of Rapid City website – hang tight! This should take 1-2 minutes."):
+        storage_context = StorageContext.from_defaults(persist_dir="savedindex")
+        index = load_index_from_storage(storage_context)
+        return index
+
+index = load_data()
+
+if "chat_engine" not in st.session_state.keys(): # Initialize the chat engine
+        st.session_state.chat_engine = index.as_chat_engine(chat_mode="condense_question", verbose=True)
+
+if prompt := st.chat_input("Your question"): # Prompt for user input and save to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+for message in st.session_state.messages: # Display the prior chat messages
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+# If last message is not from assistant, generate a new response
+if st.session_state.messages[-1]["role"] != "assistant":
+    with st.chat_message("assistant"):
+        with st.spinner("Thinking..."):
+            response = st.session_state.chat_engine.chat(prompt)
+            st.write(response.response)
+            message = {"role": "assistant", "content": response.response}
+            st.session_state.messages.append(message) # Add response to message history
